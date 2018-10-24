acb051966e539b7d730b5fc26303094f538409b9
David Edgardo Nuñez <davidedgardonu@unitec.edu>
2018-10-24 00:10:51 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,7 +1,35 @@
 #include <iostream>
+#include "colaSubway.h"
 using namespace std;
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+    ColaSubway c;
+    int cantidad=0;
+    string nombre;
+    string subway;
+    cout<<"Cantidad de subways a insertar: ";
+    cin>>cantidad;
+    for (int i = 0; i <cantidad ; ++i) {
+        cout<<"Ingrese el subway: ";
+        cin>>subway;
+        c.agregar(subway);
+    }
+    system("cls");
+    while(true) {
+        cout << "--------Bienvenido A Subway---------\n";
+        cout << "Ingrese su nombre: ";
+        cin >> nombre;
+        if(c.esVacia()) {
+            cout << "Ya no hay subways\n";
+            system("pause");
+            return 0;
+        }
+        cout << "\n -----------Factura------------\n";
+        cout << nombre << " usted compro: ";
+        c.imprimir();
+        c.eliminar();
+        cout<<"\n";
+        system("pause");
+        system("cls");
+    }
 }
